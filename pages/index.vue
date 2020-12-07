2d9cd6dd17abd34e484bf7940fb3438a6f8315ec
alexPouivet <pouivet.alexandre@gmail.com>
2020-12-07 15:26:34 +0100

    init project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <h1>#Todo</h1>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+      <p>Alexandre Pouivet @ DevChallenges.io</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
